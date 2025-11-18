1682705c882f5ee1d716aaff9eba65038459e5a7
RodrigoDevBack <rodrigomoraes3040@gmail.com>
2025-11-18 02:12:24 -0300

    refact: Ajustes para o deploy na vps

--- src/frontend/js/home.js
+++ src/frontend/js/home.js
@@ -109,7 +109,7 @@ export async function initHomePage() {
         ${(product.images != null) ? `
         <div class="thumb">
           <img 
-            src="http://127.0.0.1:5000/images_products/${product.name}/${product.images[0]}" 
+            src="http://api.singlotown.com.br/images_products/${product.name}/${product.images[0]}" 
             width="100%" height="100%" 
             alt="${product.name}" 
             style="object-fit: contain; border-radius: 8px;">
