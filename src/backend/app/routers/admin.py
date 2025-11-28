173c74ae3395bdf7413b0f84a2dc2d393b7428c1
lucaspaiva-lp <gml248@tutamail.com>
2025-11-27 22:30:26 -0300

    fix: correção do excluir produtos do gerenciamento de produtos
    Co-authored-by: RodrigoDevBack <rodrigomoraes3040@gmail.com>

--- src/backend/app/routers/admin.py
+++ src/backend/app/routers/admin.py
@@ -3,7 +3,7 @@ from typing import (
     List,
 )
 
-from fastapi import ( 
+from fastapi import (
     APIRouter,
     Depends,
     status,
@@ -14,7 +14,7 @@ from fastapi import (
 
 from security.user_depends import get_user_admin
 
-from schemas.admin import ( 
+from schemas.admin import (
     AdminCreateProductSchema,
     AdminDisableUserSchema,
     AdminUpdateProductSchema,
@@ -28,147 +28,131 @@ from service.user import UserService
 from service.product import ProductService
 
 
-admin = APIRouter(
-    tags = ['Admin'],
-    responses = {404: {'Description': 'Not found'}}
-)
+admin = APIRouter(tags=["Admin"], responses={404: {"Description": "Not found"}})
 
 
-@admin.get('/users', response_model = List[UserSchemas.UserResponseSchema])
-async def get_users(
-    depends: Annotated[str, Depends(get_user_admin)]
-    ):
+@admin.get("/users", response_model=List[UserSchemas.UserResponseSchema])
+async def get_users(depends: Annotated[str, Depends(get_user_admin)]):
     """...
-    
-    Retorna todos os usuários que estão ativos ou não ativos.
-    
-    O retorno consiste numa lista de multiplos dados de usuário.
 
-    Args:
-        depends role: Admin
+        Retorna todos os usuários que estão ativos ou não ativos.
 
-    Returns:
-    [
-        {
-            "id": 0,
-            "name": "string",
-            "gmail": "string",
-            "status_email": true,
-            "status": true,
-            "admin": true
-        }
-    ]
-    
-...
+        O retorno consiste numa lista de multiplos dados de usuário.
+
+        Args:
+            depends role: Admin
+
+        Returns:
+        [
+            {
+                "id": 0,
+                "name": "string",
+                "gmail": "string",
+                "status_email": true,
+                "status": true,
+                "admin": true
+            }
+        ]
+
+    ...
     """
     users = await AdminService.get_users()
-    
+
     users_response = AdminService.serialize_users_response(users)
-    
+
     return users_response
 
 
-@admin.patch('/disable-client-account')
+@admin.patch("/disable-client-account")
 async def disable_client_account(
-    user: AdminDisableUserSchema,
-    depends: Annotated[str, Depends(get_user_admin)]
-    ):
-    
+    user: AdminDisableUserSchema, depends: Annotated[str, Depends(get_user_admin)]
+):
     """...
-    
-    Desabilita permanentemente a conta do usuário.
-    
-    Necessita usar o gmail do usuário para desabilitar a conta permanentemente.
 
-    Returns:
-        {
-            200 OK: "Status updated"
-        }
-...
+        Desabilita permanentemente a conta do usuário.
+
+        Necessita usar o gmail do usuário para desabilitar a conta permanentemente.
+
+        Returns:
+            {
+                200 OK: "Status updated"
+            }
+    ...
     """
-    
+
     if await UserService.user_exists_by_gmail(user.gmail):
         use = await UserService.get_by_gmail(user.gmail)
         await UserService._desactivate(use)
-        
-        return {status.HTTP_200_OK : 'Status updated'}
+
+        return {status.HTTP_200_OK: "Status updated"}
 
 
-@admin.post('/add-product-image')
+@admin.post("/add-product-image")
 async def add_product_image(
-    id: int = Form(...),
-    image: UploadFile = File(...),
-    depends =  Depends(get_user_admin)
-    ):
-    
+    id: int = Form(...), image: UploadFile = File(...), depends=Depends(get_user_admin)
+):
     """...
-    
-    Adiciona a imagem ao produto cadastrado.
-    
-    Solicita:
-        {
-            "id": int,
-            "image": File
-        }
-    
-    Returns:
-        {
-            True
-        }
-...
+
+        Adiciona a imagem ao produto cadastrado.
+
+        Solicita:
+            {
+                "id": int,
+                "image": File
+            }
+
+        Returns:
+            {
+                True
+            }
+    ...
     """
-    
+
     product = await ProductService.get_product_id(id)
-    
+
     await ProductService.add_image_product(product, image)
-    
+
     return True
 
 
-@admin.post('/create-product', response_model = AdminResponseProductSchema)
+@admin.post("/create-product", response_model=AdminResponseProductSchema)
 async def create_product(
-    data: AdminCreateProductSchema,
-    depends =  Depends(get_user_admin)
-    ):
-    
+    data: AdminCreateProductSchema, depends=Depends(get_user_admin)
+):
+
     product = await ProductService.create_product(data)
-    
+
     return product
 
 
-@admin.put('/edit-product', response_model=AdminUpdateProductSchema)
-async def edit_product(
-    data: AdminUpdateProductSchema,
-    depends = Depends(get_user_admin)
-    ):
-    
+@admin.put("/edit-product", response_model=AdminUpdateProductSchema)
+async def edit_product(data: AdminUpdateProductSchema, depends=Depends(get_user_admin)):
+
     product_update = await ProductService.get_product_id(data.id)
-    
+
     product_update = await ProductService.update_product(product_update, data)
-    
+
     return product_update
 
 
-@admin.patch('/delete-product-image')
+@admin.patch("/delete-product-image")
 async def delete_product_image(
-    id: int = Form(...),
-    del_image: str = Form(...),
-    depends =  Depends(get_user_admin)
-    ):
-    
+    id: int = Form(...), del_image: str = Form(...), depends=Depends(get_user_admin)
+):
+
     product = await ProductService.get_product_id(id)
-    
+
     return await ProductService.delete_image(product, del_image)
 
 
-@admin.delete('/delete-product')
+@admin.delete("/delete-product")
 async def delete_product(
     product_id: AdminDeleteProductSchema,
-    depends: Annotated[str, Depends(get_user_admin)]
-    ):
-    
+    depends: Annotated[str, Depends(get_user_admin)],
+):
+
     product_exists = await ProductService.get_product_id(product_id.id)
-    
-    ProductService.delete_product(product_exists)
-    
+
+    await ProductService.delete_product(product_exists)
+
     return status.HTTP_200_OK
--- src/backend/app/service/product.py
+++ src/backend/app/service/product.py
@@ -8,17 +8,15 @@ from integrations.image_save import (
     move_images_a_new_path,
 )
 
+
 class ProductService:
-    @staticmethod    
+    @staticmethod
     async def get_product_id(id: int) -> Product:
         product = await Product.get_or_none(id=id)
         if not product:
-            raise HTTPException(
-                status.HTTP_404_NOT_FOUND,
-                detail='Product not found'
-            )
+            raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Product not found")
         return product
-    
+
     @staticmethod
     async def add_image_product(product: Product, image):
         if not product.images:
@@ -27,38 +25,41 @@ class ProductService:
             images = list(product.images)
 
         images.append(add_image(product.name, image))
-        
+
         product.images = images
         await product.save()
         return True
-    
+
     @staticmethod
     async def create_product(data: AdminCreateProductSchema):
         product = await Product.create(
-            name = data.name,
-            description = data.description,
-            qtd = data.qtd,
-            price = data.price,
+            name=data.name,
+            description=data.description,
+            qtd=data.qtd,
+            price=data.price,
         )
         return product
-    
+
     @staticmethod
     async def update_product(product_update: Product, data: AdminUpdateProductSchema):
         if data.name != None:
             falha = move_images_a_new_path(product_update.name, data.name)
             if falha != True:
-                raise HTTPException(status.HTTP_400_BAD_REQUEST, detail= f"Ao mover as imagens ocorreu uma falha misteriosa.")
+                raise HTTPException(
+                    status.HTTP_400_BAD_REQUEST,
+                    detail=f"Ao mover as imagens ocorreu uma falha misteriosa.",
+                )
             product_update.name = data.name
-            
+
         if data.description != None:
             product_update.description = data.description
-            
+
         if data.qtd != None:
             product_update.qtd = data.qtd
-            
+
         if data.price != None:
             product_update.price = data.price
-            
+
         await product_update.save()
         return product_update
 
@@ -66,25 +67,23 @@ class ProductService:
     async def delete_image(product: Product, del_image):
         if not product.images:
             raise HTTPException(
-                status.HTTP_404_NOT_FOUND,
-                detail= 'Product don´t have images'
+                status.HTTP_404_NOT_FOUND, detail="Product don´t have images"
             )
         else:
             images = list(product.images)
-        
+
         if delete_product_image(product.name, del_image):
             images.remove(del_image)
             product.images = images
             await product.save()
-            
+
             return True
         else:
             return False
-        
+
     @staticmethod
     async def delete_product(product_exists: Product):
         await product_exists.delete()
+        await product_exists.save()
         delete_directory_product(product_exists.name)
         return True
-    
-    
